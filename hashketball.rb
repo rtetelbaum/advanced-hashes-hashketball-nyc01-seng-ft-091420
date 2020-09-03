7cbfdf9c868e5e771040c334f1fc97cc144a0cdd
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-03 20:40:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -175,7 +175,7 @@ def player_stats(name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player_data|
       if player_data[:player_name] == name
-        player_data
+        return player_data
       end
     end
   end
