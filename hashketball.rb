2d05ccd5a9e40cd9b3b702d41df298348acc14bf
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-03 18:28:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ def num_points_scored(name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
       if player[:player_name] == name
-        return player[:points]
+        return player[:shoe]
       end
     end
   end
@@ -139,22 +139,29 @@ end
 
 def shoe_size(name)
   game_hash.each do |location, team_data|
-    team_data.each do |team_attribute, team_info|
-      if team_attribute == :players
-        team_info.each do |array|
-          array.each do |player_attribute, player_stat|
-            if player_stat == name
-              array.each do |player_attribute, player_stat|
-                if player_attribute == :shoe
-                  return player_stat
-                end
-              end
-            end
-          end
-        end
-      end
+  team_data[:players].each do |player|
+    if player[:player_name] == name
+      return player[:points]
     end
   end
+end
+  # game_hash.each do |location, team_data|
+  #   team_data.each do |team_attribute, team_info|
+  #     if team_attribute == :players
+  #       team_info.each do |array|
+  #         array.each do |player_attribute, player_stat|
+  #           if player_stat == name
+  #             array.each do |player_attribute, player_stat|
+  #               if player_attribute == :shoe
+  #                 return player_stat
+  #               end
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 def team_colors(team)
